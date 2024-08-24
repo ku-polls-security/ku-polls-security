0efeb2681029e92ca43d94c84ab2e0831fb1dd4d
TAGCH <pisit.au@ku.th>
2024-08-25 02:27:05 +0700

    Set the vote and result view

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,9 @@
 """Handles poll display, voting, and admin functions in the KU Polls app."""
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request):
@@ -25,10 +25,27 @@ def detail(request, question_id):
 
 def results(request, question_id):
     """Determine the view of the result page."""
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    """Determine the view of the vote page."""
-    return HttpResponse("You're voting on question %s." % question_id)
+    """Determine the view of the vote page.
+
+    Catch the error in the case where the choice does not exist.
+    Receive the answer, then redirect to the result page.
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(
+            reverse('polls:results', args=(question.id,))
+        )
