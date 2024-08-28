8555d00448899dc1701f4872d538ed76864d23ae
TAGCH <pisit.au@ku.th>
2024-08-29 01:55:10 +0700

    Add is_published and can_vote

--- polls/models.py
+++ polls/models.py
@@ -32,6 +32,28 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    def is_published(self):
+        """
+        Determine if the question is published.
+
+        Returns:
+            bool: True if the current date-time is on or after question’s
+            publication date, False otherwise.
+        """
+        return timezone.localtime() >= self.pub_date
+
+    def can_vote(self):
+        """
+        Determine if the question can be voted on.
+
+        Returns:
+            bool: True if voting is allowed for this question,
+            False otherwise.
+        """
+        if self.end_date:
+            return self.pub_date <= timezone.localtime() < self.end_date
+        return True
+
 
 class Choice(models.Model):
     """Model representing a choice for a specific poll question."""
