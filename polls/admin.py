6cefb50ddede123140f5a1080506ef24f38068bd
oaoak <khongnat.b@ku.th>
2024-12-14 13:02:02 +0700

    Use UUID

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,7 @@
 """Tell the admin that Question objects have an admin interface."""
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 admin.site.register(Question)
+admin.site.register(Choice)
